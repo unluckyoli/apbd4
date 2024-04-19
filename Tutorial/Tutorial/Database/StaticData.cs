3d2bca6339f2aa93e24326e5c1e7491241c27239
oli <s28526@pjwstk.edu.pl>
2024-04-19 12:39:37 +0200

    porzadkowanie

--- Tutorial/Tutorial/Database/StaticData.cs
+++ Tutorial/Tutorial/Database/StaticData.cs
@@ -8,13 +8,10 @@ public class StaticData
     public static List<Animal> animals = new List<Animal>()
     {
         new Animal(),
-        new Animal(),
-        new Animal(),
     };
 
     public static List<Visit> visits = new List<Visit>()
     {
         new Visit { Id = 1, VisitDate = DateTime.Now, AnimalId = 1, Description = "First visit", Price = 50.00m },
-        new Visit()
     };
 }
